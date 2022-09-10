17d2c71d5e1f485124d106087c3537c18554811e
Bence Kotis <bkotis@redhat.com>
2022-09-10 18:42:44 +0200

    Added a new simple test case using ls

--- main_test.go
+++ main_test.go
@@ -3,12 +3,13 @@ package main
 import(
 
 	"testing"
+	"os/exec"
 )
 
 
 func TestLinesStringCount(t *testing.T) {
-// Function to test running a simple ls
-// in parallel
+	/* Function to test the line counting function
+	*/
 
 	msg := "s1\ns2\n"
 	if linesStringCount(msg) != 2 {
@@ -16,3 +17,18 @@ func TestLinesStringCount(t *testing.T) {
 	}
 
 }
+
+func TestLs(t *testing.T) {
+	/* Function to test running a simple ls in parallel
+	*/
+
+	test_command := "go build && ./prl -j 4 -cmd 'ls {tests.txt}'"
+        cmd := exec.Command("bash","-c",test_command)
+                _,err := cmd.CombinedOutput()
+
+	if err != nil {
+		t.Error("Error returned when trying to run a simple ls in parallel.")
+	}
+
+}
+
